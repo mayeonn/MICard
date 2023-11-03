492746065754de1e6f294b54acbd9a5e92915dd3
mayeonn <mayeonn9@gmail.com>
2023-11-03 13:20:26 +0900

    Initial Commit

---
+++ MiCard/MiCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiCardApp.swift
+//  MiCard
+//
+//  Created by 김하연 on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MiCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
